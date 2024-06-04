1efc7e5b2f93dff98725d233d74248ea55389381
Mr-Prakash6381 <prakash875478@gmail>
2024-06-03 22:20:57 -0700

    feat: add the new fill

---
+++ Day-7/filter.js
@@ -0,0 +1,40 @@
+// filter () method in javascript
+// let a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let c = a.filter((b) => b % 2 == 0);
+// console.log(c);
+
+// let e = [
+//   { name: "Alice", age: 25 },
+//   { name: "Bob", age: 30 },
+//   { name: "Charlie", age: 35 },
+// ];
+// let g = e.filter((t) => t.age > 30);
+// console.log(g);
+
+// let words = ["cat", "dog", "elephant", "fish", "giraffe"];
+// let p = words.fill((k) => k.length === 4);
+// console.log(p);
+// let items = [
+//   {
+//     name: "Apple",
+//     category: "Fruit",
+//   },
+//   { name: "carrot", category: "Vegetable" },
+//   { name: "banana", category: "fruit" },
+// ];
+// let fruits = items.filter((item) => item.category === "fruit");
+// console.log(fruits);
+// let words = ["cat", "dog", "elephant", "fish", "giraffe"];
+// let word = words.filter((w) => w.startsWith("e"));
+// console.log(word);
+// let items = [
+//   {
+//     name: "Apple",
+//     category: "Fruit",
+//     prince: 100,
+//   },
+//   { name: "carrot", category: "Vegetable", prince: 50 },
+//   { name: "banana", category: "fruit", prince: 100 },
+// ];
+// let mul = items.filter((one) => one.category === "fruit" && one.prince === 10);
+// console.log(mul);
