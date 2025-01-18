8ce5ac65ee09195985018edfc5da347db23343a3
Mr--Prakash6381 <prakash875478@gmail.com>
2025-01-18 23:21:11 +0530

    Feat : Add for_loop

---
+++ Day-9/for_loop.js
@@ -0,0 +1,22 @@
+let tableFor = 2;
+for (let i = 1; i <= 10; i++) {
+    //console.log("I Love with you Programing ");
+    console.log(i + ' * ' + tableFor + ' = ' + (i * tableFor));
+}
+
+let j = 0;
+while (j <= 10) {
+    console.log(j);
+    j++
+}
+
+let names=["Sangay",'Monage',"Prabu","Visal","bala","balaMurgan","Sunil"];
+/*let len=names.length;
+console.log("len =",len);
+for (let i=0;i<=len;i++){
+    console.log(names[i]);
+}
+    */
+for(let i =0;i<names.length;i++){
+    console.log(names[i]);
+}
