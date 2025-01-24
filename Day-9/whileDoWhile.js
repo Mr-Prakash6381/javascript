c5d20f515f8fdfe3c82b01bad7c57f1c241e8c93
Mr--Prakash6381 <prakash875478@gmail.com>
2025-01-24 20:02:18 +0530

    Feat : whileDoWileLoop

---
+++ Day-9/whileDoWhile.js
@@ -0,0 +1,23 @@
+//let i = 1;
+//let tableFor = 2;
+//while (i <= 10) {
+//  console.log(i + ' * ' + tableFor + " = " + i * tableFor);
+//i++;
+//}
+let names = ["Aabu", "Parmel", "Hari", "Raj", "Sadha"];
+console.log(names.length);
+/*
+while (i < names.length) {
+    console.log(names[i]);
+    i++;
+}
+    */
+do{
+    console.log(names[i]);
+    i++;
+}
+while(i<names.length){
+    console.log("test");
+    i++;
+
+}
