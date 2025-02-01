a924547535802376d8eab22ecd0bebab4f752273
Mr--Prakash6381 <prakash875478@gmail.com>
2025-02-01 18:29:02 +0530

    Feat : Html Element Creat using JavaScript

---
+++ Day-10/htmlElementsWithJavaScript.js
@@ -0,0 +1,27 @@
+/*
+const section = document.getElementById('headerSection');
+console.log({ section })
+
+
+const heading = document.getElementsByName('heading');
+console.log(heading);
+
+
+const introContent = document.getElementsByClassName('introContent');
+console.log()
+console.log(introContent)
+console.log(introContent[0])
+console.log(introContent[1])
+console.log(introContent[2])
+console.log()
+
+
+const p = document.getElementsByTagName('p')
+console.log(p)
+
+
+const headerSection=document.querySelector('#headerSection');
+console.log({headerSection});
+*/
+const introContent=document.querySelectorAll('.introContent');
+console.log(introContent[0]);
