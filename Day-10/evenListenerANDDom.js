d30bee8546c5549066627059596037eeb8b32c4e
Mr--Prakash6381 <prakash875478@gmail.com>
2025-02-01 18:30:36 +0530

    Feat : even List Ener And DOM

---
+++ Day-10/evenListenerANDDom.js
@@ -0,0 +1,10 @@
+isModelOpen=false;
+const Section1Addbutton = document.querySelector('#Section1Addbutton');
+const List = document.querySelector('#myList');
+const Section2 = document.querySelector('#Section2')
+Section2.Style.display='none';
+
+addButton.onclick = () => {}
+function toggleModel(){
+    isModelOpen=!isModelOpen
+} 
